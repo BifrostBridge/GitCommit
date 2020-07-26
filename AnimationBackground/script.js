44023f8cb2d98f15de154955c43d87d9cdba5260
David <JavaScriptDi@tutanota@com>
2020-07-27 00:32:48 +0300

    Last Commit

--- AnimationBackground/script.js
+++
@@ -1,5 +0,0 @@
-const body = document.body
-body.onclick = function(event) {
-    console.log(`X:`,event.clientX)
-    console.log(`Y:`, event.clientY)
-}
---
+++ Event/script.js
@@ -0,0 +1,14 @@
+const body = document.body
+body.onclick = function(event) {
+    console.log(`X:`,event.clientX)
+    console.log(`Y:`, event.clientY)
+}
+const btn = document.getElementById('btn')
+const p = document.getElementById('Paragraph')
+btn.onclick = function() {
+    p.style.background = 'red'
+    p.style.color = 'blue'
+    p.style.fontSize = '30px'
+    p.style.transition = 'all 1s linear'
+    p.style.textAlign = 'center'
+}
