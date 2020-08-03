f6efa1cbe77abd59c9f182889a4137790a0af931
David <JavaScriptDi@tutanota.com>
2020-08-03 15:47:43 +0300

    New commit

--- ClearJavaScript/script/context.js
+++ ClearJavaScript/script/context.js
@@ -1,9 +1,45 @@
 function showMessage() {
     console.log('Hello', this)
 }
+function sum() {
+    console.log('Summa of the nubmers')
+}
 const person = {
     name: 'David',
     age: 26,
     sayHello: showMessage,
-    sayHelloWindow: showMessage.bind(window)
+    sayHelloWindow: showMessage.bind(this),
+    logInfo: function(job,phone) {
+        console.group(`${this.name} info:`)
+        console.log(`Name is ${this.name}`)
+        console.log(`Age is ${this.age}`)
+        console.log(`Job is ${job}`)
+        console.log(`Phone is a ${phone}`)
+        console.groupEnd()
+    }
 }
+
+const lena  = {
+    name: 'Elena',
+    age: 23,   
+}
+
+
+person.logInfo.bind(lena,'FrontEnd', '8-999-123-12-23')() // <--- Передает первый аргумент контекст объекта Лена последующие параметры.Нужно вызивать ее
+person.logInfo.call(lena,'Frondend','8-999-221-32-13') // Сразу вызивает по сравнению байнд
+person.logInfo.apply(lena,['Frondend','8-999-221-32-13']) //Передача параметров в массиве 
+
+//=========================================
+
+const arr = [1, 2, 3, 4, 5]
+// function myltBy(arr,n) {
+//     return arr.map(function(i) {
+//         return n * i
+//     })
+// }
+Array.prototype.multBy = function(n) {
+    return this.map(function(i) {
+        return i * n
+    })
+}
+console.log(arr.multBy(10))
---
+++ ClearJavaScript/script/zamikanie.js
@@ -0,0 +1,47 @@
+// function createCalcFunction(n) {
+//     return function() {
+//         console.log(1000 * n)
+//     }
+// }
+// const calc = createCalcFunction(42)
+// calc()
+// function createIncrementor(n) {
+//     return (num) => { //arrow function  
+//         return n + num
+//     }
+// }
+// const addOne = createIncrementor(1)
+// const addTen = createIncrementor(10)
+// console.log(addTen(10))
+// console.log(addOne(10))
+// console.log(addOne(12))
+// console.log(addOne(13))
+// console.log(addOne(14))
+
+function urlGenerator(domain) {
+    return function(url) {
+        return `https://${url}.${domain}`
+    }
+}
+
+const ru  = urlGenerator('ru')
+const org = urlGenerator('org')
+const com = urlGenerator('com')
+console.log(ru('google'))
+console.log(ru('yandex'))
+console.log(ru('yahoo'))
+console.log(ru('gmail'))
+console.log(org('tor'))
+function logPerson() {
+    console.log(`Person ${this.name} ${this.age} ${this.job}`)
+}
+const person1 = {name: 'David', age: 23, job: 'FrontEnd'}
+const person2 = {name: 'Ignat', age: 24, job: 'Desing'}
+
+function bind(contex,fn) {
+    return function(...args) {
+        fn.apply(contex,args)
+    }
+}
+bind(person1,logPerson)
+bind(person2,logPerson)
